1c2efec5c5c825051e61dbd568ba79a5c3df910c
Michał Maćkowski <m_mackowski@yahoo.com>
2017-07-25 14:10:31 +0200

    added framesReceived verification test
    Signed-off-by: Michał Maćkowski <m_mackowski@yahoo.com>

--- tests/candevicetest.cpp
+++ tests/candevicetest.cpp
@@ -1,14 +1,17 @@
 
-#include <candevice/candevice.h>
 #define CATCH_CONFIG_RUNNER
 #include <fakeit.hpp>
+#include <QtSerialBus/QCanBusFrame>
 
 #include "candevice/candeviceinterface.hpp"
 #include "candevice/canfactory.hpp"
 #include "candevice/candeviceqt.hpp"
 #include "candevice/canfactoryqt.hpp"
-
+#include "candevice/candevice_p.h"
 #include "log.hpp"
+
+#define private public
+#include <candevice/candevice.h>
 
 std::shared_ptr<spdlog::logger> kDefaultLogger;
 
@@ -38,22 +41,70 @@ TEST_CASE("Initialization succedded", "[candevice]")
     CHECK(canDevice.init("", "") == true);
 }
 
-TEST_CASE("Start failed - canDevice is not initialized","[candevice]")
+TEST_CASE("Start failed","[candevice]")
 {
+    using namespace fakeit;
     Mock<CanFactoryInterface> factoryMock;
-    When(Method(factoryMock, create)).Return(nullptr);
+    Mock<CanDeviceQt> deviceQtMock;
+
+    When(Method(factoryMock, create)).Return( nullptr );
+    CanDevice canDeviceNullPtr{ factoryMock.get() };
+    canDeviceNullPtr.init("","");
+    CHECK(canDeviceNullPtr.start() == false);
+
+    When(Method(deviceQtMock,connectDevice)).Return(false);
+    Fake(Dtor(deviceQtMock),
+         Method(deviceQtMock, framesWritten),
+         Method(deviceQtMock, framesReceived),
+         Method(deviceQtMock, errorOccurred));
+
+    factoryMock.Reset();
+    When(Method(factoryMock,create)).Return( &(deviceQtMock.get()) );
+
     CanDevice canDevice{ factoryMock.get() };
+    canDevice.init("","");
     CHECK(canDevice.start() == false);
 }
 
-TEST_CASE("Start failed - could not connect to device","[candevice]")
+TEST_CASE("Start succeedded","[candevice]")
 {
-    Mock<CanDeviceQt> device;
-    Mock<CanFactoryQt> canFactory;
-    When(Method(device, connectDevice)).Return(false);
-    When(Method(canFactory, create)).Return(&device.get());
-    CanDevice canDevice(canFactory.get());
-    CHECK(canDevice.start() == false);
+    using namespace fakeit;
+    Mock<CanFactoryInterface> factoryMock;
+    Mock<CanDeviceQt> deviceQtMock;
+
+    When(Method(factoryMock, create)).Return( &(deviceQtMock.get()) );
+
+    Fake(Dtor(deviceQtMock),
+         Method(deviceQtMock, framesWritten),
+         Method(deviceQtMock, framesReceived),
+         Method(deviceQtMock, errorOccurred));
+
+    When(Method(deviceQtMock, connectDevice)).Return(true);
+
+    CanDevice canDevice{ factoryMock.get() };
+    canDevice.init("","");
+    CHECK(canDevice.start() == true);
+}
+
+TEST_CASE("framesReceived verification","[candevice]")
+{
+    Mock<CanFactoryInterface> factoryMock;
+    Mock<CanDeviceQt> deviceQtMock;
+    QCanBusFrame frame;
+
+    When(Method(factoryMock, create)).Return( &(deviceQtMock.get()) );
+    Fake(Dtor(deviceQtMock),
+         Method(deviceQtMock, framesWritten),
+         Method(deviceQtMock, framesReceived),
+         Method(deviceQtMock, errorOccurred));
+    When(Method(deviceQtMock, framesAvailable)).Return(10,9,8,7,6,5,4,3,2,1,0);
+    When(Method(deviceQtMock, readFrame)).Return(10_Times(frame));
+
+    CanDevice canDevice{ factoryMock.get() };
+    canDevice.init("","");
+    canDevice.framesReceived();
+    Verify(Method(deviceQtMock, framesAvailable)).Exactly(11);
+    Verify(Method(deviceQtMock, readFrame)).Exactly(10);
 }
 
 int main(int argc, char* argv[])
@@ -63,6 +114,6 @@ int main(int argc, char* argv[])
     if (haveDebug) {
         kDefaultLogger->set_level(spdlog::level::debug);
     }
-    cds_debug("Staring unit tests");
+    cds_debug("Startling unit tests");
     return Catch::Session().run(argc, argv);
 }
